33b959e5bff5ea70b24cf775079a0941cb1390c0
Ciro Sorre <38884767+Ciro23@users.noreply.github.com>
2020-06-07 23:44:22 +0200

    Update user-delete-submit.php

--- includes/users/user-delete-submit.php
+++ includes/users/user-delete-submit.php
@@ -7,14 +7,13 @@ if (isset($_POST['user-delete-submit'])) {
 
     // hides everything about the user in the db
     mysqli_query($db, $sql = "UPDATE posts SET status = 0 WHERE author = $idUser");
-    mysqli_query($db, $sql = "UPDATE posts SET status = 0 WHERE author = $idUser");
     mysqli_query($db, $sql = "UPDATE comments SET status = 0, content = '[deleted]' WHERE author = $idUser");
     mysqli_query($db, $sql = "UPDATE users SET status = 0, email = '[deleted]', password = '[deleted]' WHERE id = $idUser");
 
     // deletes the cookies
     session_destroy();
 
-    header("Location: /404");
+    header("Location: /");
 } else {
     header("Location: /404");
 }
